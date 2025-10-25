df660350955b03d8cb7afcab83a6fe609457fbbb
Kevin R <kevinru2005@gmail.com>
2025-10-25 01:50:20 -0700

    poggers stuffz

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,8 @@
+import uuid
+import os
 from dotenv import load_dotenv
 from elasticsearch import Elasticsearch
 from flask import Flask, request, jsonify
-import os
 
 load_dotenv()
 
@@ -15,6 +16,7 @@ client = Elasticsearch(
 
 app = Flask(__name__)
 
+
 def update_view_count(doc_id):
     """
     Atomically increments the 'view_count' field for the specified document.
@@ -24,28 +26,29 @@ def update_view_count(doc_id):
         response = client.update(
             index=INDEX_NAME,
             id=doc_id,
-            
             script={
                 "source": "ctx._source.view_count = ctx._source.view_count + params.inc",
-                "params": {
-                    "inc": 1
-                }
+                "params": {"inc": 1},
             },
-            upsert={
-                "view_count": 1 
-            }
+            upsert={"view_count": 1},
         )
-        
-        return jsonify({
-            "message": f"View count updated. Result: {response['result']}",
-            "id": doc_id
-        }), 200
+
+        return (
+            jsonify(
+                {
+                    "message": f"View count updated. Result: {response['result']}",
+                    "id": doc_id,
+                }
+            ),
+            200,
+        )
 
     except Exception as e:
         print(f"Update error: {e}")
         return jsonify({"message": "Failed to update view count."}), 500
 
-@app.route("/api/upload_place", methods=["GET","POST"])
+
+@app.route("/api/upload_place", methods=["GET", "POST"])
 def upload_event():
     if request.is_json:
         data = request.get_json()
@@ -55,7 +58,7 @@ def upload_event():
         long = float(data.get("lon"))
         summary = data.get("summary")
         place_id = data.get("place_id")
-        
+
         if not all([lat, long, summary, place_id]):
             return (
                 jsonify(
@@ -63,12 +66,12 @@ def upload_event():
                 ),
                 400,
             )
-            
-        # if doc exists just update view count 
+
+        # if doc exists just update view count
         doc_exists = client.exists(id=place_id, index=INDEX_NAME)
         if doc_exists:
             return update_view_count(place_id)
-        
+
         doc = {
             "location": {
                 "lat": lat,
@@ -77,16 +80,29 @@ def upload_event():
             "place_id": place_id,
             "place_summary": summary,
             "doc_type": "place",
-            "view_count": 1
+            "view_count": 1,
         }
         try:
-            response = client.index(
-                index=INDEX_NAME, 
-                id=place_id,
-                document=doc
-            )
-            return jsonify({"message":"Doc indexed succesfully"}), 200 
+            response = client.index(index=INDEX_NAME, id=place_id, document=doc)
+            return jsonify({"message": "Doc indexed succesfully"}), 200
         except Exception as e:
             print("Indexing error %s", e)
             return jsonify({"message": "Failed to index doc"}), 500
-    return jsonify({"message": "expected JSON"}), 500   
+    return jsonify({"message": "expected JSON"}), 500
+
+
+@app.route("/api/add_user", methods=["GET", "POST"])
+def add_user():
+    # for future kev: do we even need anything here from frontend fr? 
+    user_uuid = str(uuid.uuid4())
+    doc = {
+        "doc_type": "user",
+        "uuid": user_uuid,
+        # seb gotta do some work here fr fr
+    }
+    try:
+        response = client.index(index=INDEX_NAME, document=doc)
+        return jsonify({"message": "user created succesfully, uuid: " + user_uuid}), 400
+    except Exception as e:
+        print("oopsie woopsie something went wrong: %s", e)
+        return jsonify({"message": "Something went wrong"}), 400
